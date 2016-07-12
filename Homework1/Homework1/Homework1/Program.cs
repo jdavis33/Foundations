dbd22428b549fb13e34b993c16d2b99a297e567f
jdavis33 <Vash2121@hotmail.com>
2016-07-12 16:22:38 -0700

    Homework Assignment 1

---
+++ Homework1/Homework1/Homework1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_App_Project_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("All Work and No Play Makes Jeremy A Dull Boy");
+            Console.ReadLine();
+        }
+    }
+}
