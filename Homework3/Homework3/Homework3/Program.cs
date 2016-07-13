918253ca998ca2897d84494ef1b09e3a35e7af9d
jdavis33 <Vash2121@hotmail.com>
2016-07-12 23:09:52 -0700

    Correction of my bad math

--- Homework3/Homework3/Homework3/Program.cs
+++ Homework3/Homework3/Homework3/Program.cs
@@ -10,11 +10,14 @@ namespace Homework3
     {
         static void Main()
         {
+            //Enter Seconds Here
             int x = 34000;
+            //Enter Seconds Here
+
             int s = x;
             int m = x / 60;
-            int h = x / 600;
-            int d = x / 6000;
+            int h = x / 3600;
+            int d = x / 86400;
 
             Console.WriteLine("{0} seconds", s);
             Console.WriteLine("{0} minutes", m);
