5fe65e8982e272f35fb8191ceda0afcbb931dc67
jdavis33 <Vash2121@hotmail.com>
2016-07-12 22:59:08 -0700

    Homework Assignment 3

--- Homework3/Homework3/Homework3/Program.cs
+++ Homework3/Homework3/Homework3/Program.cs
@@ -8,8 +8,20 @@ namespace Homework3
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int x = 34000;
+            int s = x;
+            int m = x / 60;
+            int h = x / 600;
+            int d = x / 6000;
+
+            Console.WriteLine("{0} seconds", s);
+            Console.WriteLine("{0} minutes", m);
+            Console.WriteLine("{0} hours", h);
+            Console.WriteLine("{0} days", d);
+            Console.ReadLine();
+
         }
     }
 }
