596127d05deefb3cacd28577a7c7643cb99ac539
jdavis33 <Vash2121@hotmail.com>
2016-07-19 15:55:43 -0700

    Updated per Dan's latest comments.

--- Homework3/Homework3/Homework3/Program.cs
+++ Homework3/Homework3/Homework3/Program.cs
@@ -10,19 +10,40 @@ namespace Homework3
     {
         static void Main()
         {
-            //Enter Seconds Here
-            int x = 34000;
-            //Enter Seconds Here
+            //Enter Seconds Here//
+            int x = 234833;
+            //Enter Seconds Here//
 
-            int s = x;
-            int m = x / 60;
-            int h = x / 3600;
+            //convert to days//
             int d = x / 86400;
 
-            Console.WriteLine("{0} seconds", s);
-            Console.WriteLine("{0} minutes", m);
-            Console.WriteLine("{0} hours", h);
-            Console.WriteLine("{0} days", d);
+            //find the remainder of seconds//
+            int dsr = (d * 86400 - x);
+            
+            
+            //convert remaining seconds to hours//
+            int h = dsr / 3600;
+           
+
+            //find the remainder of seconds//
+            int hsr = (h * 3600 - dsr);
+
+            //convert remaining seconds to minutes//
+            int m = hsr / 60;
+
+            //find the remainder of seconds//
+            int msr = (m * 60 - hsr);
+
+
+            //write converted time to console window//
+            if (d < 0) { Console.WriteLine("{0} days", d * -1); }
+            if (d >= 0) { Console.WriteLine("{0} days", d); }
+            if (h < 0) { Console.WriteLine("{0} hours", h * -1); }
+            if (h >= 0) { Console.WriteLine("{0} hours", h); }
+            if (m < 0) { Console.WriteLine("{0} minutes", m * -1); }
+            if (m >= 0) { Console.WriteLine("{0} minutes", m); }
+            if (msr < 0) { Console.WriteLine("{0} seconds", msr * -1); }
+            if (msr >= 0) { Console.WriteLine("{0} seconds", msr); }
             Console.ReadLine();
 
         }
