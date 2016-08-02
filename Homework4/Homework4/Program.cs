7b5e86d2fdb6262a2c470e1e54a093ac30961ea0
jdavis33 <Vash2121@hotmail.com>
2016-08-02 01:17:47 -0700

    I could use some help on this one.  Trying to figure out how to use uint to expand the number of factorials the program can calculate but I keep running into errors.  Also curious how I might loop the program back to the start after the error message or factorials are calculated instead of instructing the user to press enter and restart the program.  I had thought about using the while statements or a go to statement, but I'm not 100% sure that will work.

--- Homework4/Homework4/Program.cs
+++ Homework4/Homework4/Program.cs
@@ -1,15 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Homework4
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("Enter An Integer Number Here (Reminder - integers must be greater than 0):");
+            string n = Console.ReadLine();
+            int x = int.Parse(n);
+            int c;
+            int sum = 1;
+
+            if (x < 0)
+            {
+                Console.WriteLine("Please enter an integer value greater than or equal to 0");
+                Console.WriteLine("Press Enter to exit and try again.");
+            }
+
+            else if (x == 0)
+            {
+                Console.WriteLine("0! = 0");
+                Console.WriteLine("Press Enter to exit and enter another value.");
+            }
+
+            else if (x > 0)
+                {
+                    for (c = 1; c <= x; c++)
+                    {
+                        int a = sum * c;
+
+                        sum = a;
+
+                        Console.WriteLine("{0}! = {1}", c, a);
+                    }
+
+                Console.WriteLine("Press Enter to exit and enter another value.");
+                }
+
+                Console.Read();
         }
     }
 }
